7e6e71fb4e4a073c17d55f2333b1f32f0ddc3986
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2022-08-22 09:18:55 +0200

    Initial Upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from discord.ext.commands import Bot
+from discord.ext import commands
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = Bot(command_prefix='>', intents=intents) # The prefix used for the bot. 
+
+
+@bot.event
+async def on_ready():
+	"""
+ 	This event tells us when the bot is online and ready, as well as set's a status on the bot. 
+ 	"""
+	await bot.change_presence(activity = discord.Activity(type = discord.ActivityType.watching, name = 'you develop me.'))
+	print('Up and running.')
+
+	
+@bot.command()
+async def hi(ctx):
+    await ctx.send("Oh, Hi there.")
+
+
+if __name__ == "__main__":
+	"""
+  	This prevents replit from blocking us with too many requests.
+	If the system gets req blocked, we kill the system and jump to a new IP
+  	"""
+	try:
+		bot.run(os.environ["TOKEN"])
+	except:
+		os.system("kill 1")
---
+++ readme.md
@@ -0,0 +1,7 @@
+## HephaestusBot
+
+HephaestusBot is a Discord.py bot developed by the members of Practical Python. 
+
+The bot is free, and open source. Anyone is welcome to contribute, however this bot will be kept fairly basic, as it is used as a learning tool. 
+
+Feel free to check out our other projects at https://xarlos89.github.io/PracticalPython/
