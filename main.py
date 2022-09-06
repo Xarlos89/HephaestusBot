53f8a06d50d06f1a82bc2243d1c3786270bb2aa9
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2022-09-06 19:45:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
 import discord
+from discord import Member
+from discord.ext import commands
 from discord.ext.commands import Bot
 
-intents = discord.Intents.default()
-bot = Bot(command_prefix='>', intents=intents, help_command=None)
+# intents = discord.Intents.default()
+bot = Bot(command_prefix='>')
+bot.remove_command("help")
 
 
 @bot.event
 async def on_ready():
-	"""
- 	This event tells us when the bot is online and ready, as well as set's a status on the bot. 
- 	"""
 	await bot.change_presence(activity = discord.Activity(type = discord.ActivityType.watching, name = 'you develop me.'))
 	print('Up and running.')
 
@@ -21,11 +21,6 @@ async def hi(ctx):
 
 
 if __name__ == "__main__":
-	"""
-  	This prevents replit from blocking us with too many requests.
-	If the system gets req blocked, we kill the system and jump to a new IP
-  	"""
-	try:
-		bot.run(os.environ["TOKEN"])
-	except:
-		os.system("kill 1")
+
+	bot.run(os.environ["TOKEN"])
+
