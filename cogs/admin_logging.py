7b6a42516a6752530a4e2ce4bf602462b32623b7
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2022-11-23 18:43:54 +0100

    just moving this over

--- cogs/admin_logging.py
+++ cogs/admin_logging.py
@@ -5,50 +5,49 @@ from datetime import datetime
 from asyncio import sleep
 from __main__ import bot
 
-
-
-
 class logging(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-
-
-#################################								
-#								#
-#			Channel ID's		#
-#								#
-#################################
-
-		# message_delete and message_edit
-		channel_message_log = 954023390375710751
-
-		# name_change and avatar_change
-		channel_user_log_admin = 953552502937243679
-
-		# on_join and on_remove
-		channel_user_log_public = 953543179133665380
-
-		# on_join_unverified and on_remove_unverified
-		channel_verification_log = 1044302239616991242
-
 
 #-------------------------------#
 #								#
 #   channel_verification_log	#
 #								#
 #-------------------------------#
-
 	@commands.Cog.listener()
+	###### On new join, do this
 	async def on_member_join(self, member):
-		if 'Needs Approval' in [role.name for role in member.roles]:
-			logs_channel = await bot.fetch_channel(channel_verification_log) # ADMIN user log
-			embed = discord.Embed(title=''
-				, description=f'{member.mention} joined, but has not verified.'
-				, color=discord.Color.yellow())
-			await logs_channel.send(embed=embed)
+		# Add verification role
+		await member.add_roles(member.guild.get_role(1043624460168728606))
 
+		# Log unverified join
+		logs_channel = await bot.fetch_channel(1043621605940674580) # ADMIN user log
+		embed = discord.Embed(title=''
+			, description=f'{member.mention} joined, but has not verified.'
+			, color=discord.Color.yellow())
+		await logs_channel.send(embed=embed)
+
+		# Send Welcome
+		guild = member.guild
+		welcome_message = f"""
+Hi there, {member.mention}
+I'm Zorak, the moderatior of {guild.name}.
+
+We are very happy that you have decided to join us.
+Before you are allowed to chat, you need to verify that you aren't a bot.
+Dont worry, it's easy. Just go to {bot.get_channel(1031644670436061327).mention} and click the green button.
+
+After you do, all of {guild.name} is availibe to you. Have a great time :-)
+"""
+		#Send Welcome Message
+		await member.send(welcome_message)
+		time_unverified_kick = 5 # 1 hour
+		await sleep(time_unverified_kick)
+
+		# Start verification timer
+		if 'Needs Approval' in [role.name for role in member.roles]:
 			# Kick timer, in seconds.
-			time_unverified_kick = 3600
+			time_unverified_kick = 5 # 1 hour
 			await sleep(time_unverified_kick)
 
 			if 'Needs Approval' in [role.name for role in member.roles]:
@@ -59,6 +58,25 @@ class logging(commands.Cog):
 				await logs_channel.send(embed=embed)
 
 				await member.kick(reason="Did not verify.")
+
+
+
+#-------------------------------#
+#								#
+#    channel_user_log_public	#
+#								#
+#-------------------------------#
+
+
+	@commands.Cog.listener()
+	async def on_member_remove(self, member):
+		if 'Needs Approval' in [role.name for role in member.roles]:
+			return
+		else:
+			# logs_channel = await bot.fetch_channel(953543179133665380) # Welcome channel
+			logs_channel = await bot.fetch_channel(1043967844934762556) # Welcome channel
+			embed = discord.Embed(title='', description=f'{member.name} has left us.', color=discord.Color.red())
+			await logs_channel.send(embed=embed)
 
 #-------------------------------#
 #								#
@@ -78,7 +96,13 @@ class logging(commands.Cog):
 			embed.add_field(name='Before', value=before.nick, inline=True)
 			embed.add_field(name='After', value=after.mention, inline=True)
 
-			logs_channel = await bot.fetch_channel(channel_user_log_admin) # ADMIN user log
+			logs_channel = await bot.fetch_channel(1044331443884654692) # ADMIN user log
+			await logs_channel.send(embed=embed)
+
+		# Verification success logging	
+		elif 'Needs Approval' in [role.name for role in before.roles] and 'Needs Approval' not in [role.name for role in after.roles]:
+			logs_channel = await bot.fetch_channel(1043967844934762556) # user
+			embed = discord.Embed(title='', description=f'{after.mention}, human number {after.guild.member_count} has joined.', color=discord.Color.dark_green())
 			await logs_channel.send(embed=embed)
 
 	@commands.Cog.listener()
@@ -93,7 +117,7 @@ class logging(commands.Cog):
 				, value=before.mention
 				, inline=True)
 
-			logs_channel = await bot.fetch_channel(channel_user_log_admin) # ADMIN user log
+			logs_channel = await bot.fetch_channel(1044331443884654692) # ADMIN user log
 			await logs_channel.send(embed=embed)
 
 
@@ -108,7 +132,7 @@ class logging(commands.Cog):
 		author = message.author
 
 		embed = discord.Embed(title=f'<:red_circle:1043616578744357085> Deleted Message'
-			, description=f'Deleted by {message.author.mention}\nIn {message_after.channel.mention}'
+			, description=f'Deleted by {message.author.mention}\nIn {message.channel.mention}'
 			, color=discord.Color.dark_red()
 			, timestamp=datetime.utcnow())
 		embed.set_thumbnail(url=author.avatar)
@@ -116,7 +140,7 @@ class logging(commands.Cog):
 			, value=message.content
 			, inline=True)
 
-		logs_channel = await bot.fetch_channel(channel_message_log) # ADMIN message log
+		logs_channel = await bot.fetch_channel(1043607758685089932) # ADMIN message log
 		await logs_channel.send(embed=embed)
 
 	@commands.Cog.listener()
@@ -137,7 +161,7 @@ class logging(commands.Cog):
 				, value=message_after.content
 				, inline=True)
 
-			logs_channel = await bot.fetch_channel(channel_message_log) # ADMIN message log
+			logs_channel = await bot.fetch_channel(1043607758685089932) # ADMIN message log
 			await logs_channel.send(embed=embed)
 
 
